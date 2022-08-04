1a6e2a1dc610f858f8ff319fea0ab6d5a4dbae3a
Svampkant <pettersson.kungn@gmail.com>
2022-08-04 02:03:41 +0200

    end of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -27,19 +28,31 @@ namespace API.Controllers
         }
 
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers(){
-        var users = await _userRespository.GetMembersAsync();
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers(){
+            var users = await _userRespository.GetMembersAsync();
 
-        return Ok(users);
-    }
-    
-    [HttpGet("{username}")]
-    public async Task<ActionResult<MemberDto>> GetUser(string username){
-
-        return await _userRespository.GetMemberAsync(username);
+            return Ok(users);
+        }
         
-    }
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username){
+
+            return await _userRespository.GetMemberAsync(username);
+            
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto){
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRespository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRespository.Update(user);
+            if(await _userRespository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }
 
     }
 }
